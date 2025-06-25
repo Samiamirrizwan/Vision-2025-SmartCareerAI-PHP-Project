742d9f3bdafa3daf4d8ecfd0a76b389f6cf3b5ba
Samiamirrizwan <161690252+Samiamirrizwan@users.noreply.github.com>
2025-06-26 00:08:29 +0500

    Update header.php

--- includes/header.php
+++ includes/header.php
@@ -2,7 +2,7 @@
 <html> 
     <head> 
         <title>SmartCareerAI</title> 
-        <link rel="stylesheet" href="assets/style.css"> 
+        <link rel="stylesheet" href="assets/css/style.css"> 
     </head> 
     <body> 
         <header> 
