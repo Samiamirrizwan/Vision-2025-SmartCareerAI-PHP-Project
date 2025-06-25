ac956b89b4490194f08c3aa74809b01730b4aeff
Samiamirrizwan <161690252+Samiamirrizwan@users.noreply.github.com>
2025-06-26 00:01:49 +0500

    Create header.php

---
+++ includes/header.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html> 
+<html> 
+    <head> 
+        <title>SmartCareerAI</title> 
+        <link rel="stylesheet" href="assets/style.css"> 
+    </head> 
+    <body> 
+        <header> 
+            <h1>SmartCareerAI</h1> 
+            <nav> 
+                <a href="index.php">Home</a> 
+                <a href="login.php">Login</a> 
+                <a href="register.php">Register</a> 
+            </nav> 
+        </header>
