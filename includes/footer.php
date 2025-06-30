35e0acc069b7899f4381201dcc944bb73c53b348
Samiamirrizwan <161690252+Samiamirrizwan@users.noreply.github.com>
2025-06-30 20:31:55 +0500

    Update footer.php

--- includes/footer.php
+++ includes/footer.php
@@ -1,5 +1,6 @@
-<footer> 
-    <p>&copy; 2025 SmartCareerAI. All rights reserved.</p> 
-</footer> 
-</body> 
+  <footer>
+    <p>&copy; 2025 SmartCareerAI. All rights reserved.</p>
+  </footer>
+  <script src="assets/js/script.js"></script>
+</body>
 </html>
