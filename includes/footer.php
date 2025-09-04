2bc908a4e0667b896cf0e5040f2e1cc8bca1f784
Sami Amir Rizwan <161690252+Samiamirrizwan@users.noreply.github.com>
2025-09-05 01:50:25 +0500

    Updating footer.php embedded structure

--- includes/footer.php
+++ includes/footer.php
@@ -40,8 +40,8 @@
                     </ul>
                     <div class="flex space-x-4">
                         <a href="https://www.facebook.com/working4happiness/" class="text-2xl hover:text-blue-500 transition"><i class="fab fa-facebook"></i></a>
-                        <a href="#" class="text-2xl hover:text-blue-400 transition"><i class="fa-brands fa-x-twitter"></i></a>
-                        <a href="#" class="text-2xl hover:text-purple-500 transition"><i class="fab fa-instagram"></i></a>
+                        <a href="https://twitter.com/SmartCareerAi/status/1743096488300106013" class="text-2xl hover:text-blue-400 transition"><i class="fa-brands fa-x-twitter"></i></a>
+                        <a href="https://www.instagram.com/corementor.in/" class="text-2xl hover:text-purple-500 transition"><i class="fab fa-instagram"></i></a>
                     </div>
                 </div>
             </div>
